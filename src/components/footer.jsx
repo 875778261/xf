b0182fe20446ec5f0ad4e6e6228041906ccdc9da
875778261 <58726532+875778261@users.noreply.github.com>
2022-03-27 12:39:14 +0800

    Delete src directory

--- src/components/footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Layout } from 'antd';
-const { Footer } = Layout;
-class Footer1 extends React.Component {
-    render() {
-        return <Footer>myFooter</Footer>
-    }
-}
-export default Footer1
